e3a733feedfc86f722ba5d4e1a23015f32dd1338
August Birk Olsen <august.birk.olsen@gmail.com>
2017-01-08 23:49:39 +0100

    Add files via upload

--- Website/website01/js/main.js
+++ Website/website01/js/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
     $('#Header').css('height', $(window).height()*0.6+'px');
+    $('#MainContent').css('min-height', $(window).height()+'px');
 });
 
 $(window).resize(function () {
     $('#Header').css('height', $(window).height()*0.6+'px');
+    $('#MainContent').css('min-height', $(window).height()+'px');
 });
+
